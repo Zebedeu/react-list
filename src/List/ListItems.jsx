135e90413e9cbef343a71a4cd24ba7fdd0d3a646
Zebedeu <marciozebedeu@gmail.com>
2020-02-17 02:09:52 +0100

    add/add-flip-move

--- src/List/ListItems.jsx
+++ src/List/ListItems.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './listItems.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import FlipMove from 'react-flip-move';
+
+
 function ListItems(props) {
     const items = props.items;
     const listItems = items.map( item => {
@@ -26,7 +29,11 @@ function ListItems(props) {
     })
 
     return (
-    <div>{listItems}</div>
+    <div>
+        <FlipMove duration={300} easing="ease-in-out">
+            {listItems}
+        </FlipMove>
+    </div>
     )
     
 }
